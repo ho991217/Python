4d8e44a5e3d9008697eb2376e8ca8981994b694a
ho991217 <ho991217@naver.com>
2021-12-02 14:23:49 +0900

    20211130ProgrammersAlgorithm

--- BOJ/Algorithm/Sorting/[2751] Sorting Number 2.py
+++ BOJ/Algorithm/Sorting/[2751] Sorting Number 2.py
@@ -3,28 +3,52 @@ from collections import deque
 
 N = int(sys.stdin.readline())
 
-
 numbers = [int(sys.stdin.readline()) for _ in range(N)]
+numbers.sort()
 
-# def merge(arr):
-#     if len(arr) <= 1:
-#         return arr
+# def nRecQsort(arr, size = 5):
+#     stack = deque()
+#     leftIdx = 0
+#     rightIdx = size - 1
 #
-#     left, right = [], []
-#     mid = arr[len(arr) // 2]
+#     stack.append(rightIdx)
+#     stack.append(leftIdx)
 #
-#     for num in arr:
-#         if num <= mid:
-#             left.append(num)
-#         else:
-#             right.append(num)
 #
-#     return merge(left) + merge(right)
-
-
-
-
-
+#     while (stack):
+#             leftIdx = stack.pop()
+#             rightIdx = stack.pop()
+#
+#             if (rightIdx - leftIdx + 1 > 1):
+#                 pivot = arr[rightIdx]
+#                 i = leftIdx - 1
+#                 j = rightIdx
+#
+#                 while True:
+#                     i += 1
+#                     while (arr[i] < pivot):
+#                         i += 1
+#
+#                     j -= 1
+#                     while (arr[j] > pivot):
+#                         j -= 1
+#
+#                     if i >= j:
+#                         break
+#
+#                     arr[i], arr[j] = arr[j], arr[i]
+#
+#
+#                 arr[i], arr[rightIdx] = arr[rightIdx], arr[i]
+#
+#                 stack.append(rightIdx)
+#                 stack.append(i + 1)
+#                 stack.append(i - 1)
+#                 stack.append(leftIdx)
+#
+#     return arr
+#
+# numbers = nRecQsort(numbers, N)
 
 for i in numbers:
     print(i)
