5706b91b11e7efcd9aabad2524329f09484d0f66
ho991217 <ho991217@naver.com>
2021-12-05 19:56:35 +0900

    202112054init

---
+++ BOJ/Algorithm/[10816] NumberCards2.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+# hand = [int(i) for i in sys.stdin.readline().rstrip().split(' ')]
+hand = dict()
+
+# for i in range(N):
+    
+
+
+# def find(arr, key):
+#     arr.sort()
+#     mid = arr[len(arr) // 2]
+
+
