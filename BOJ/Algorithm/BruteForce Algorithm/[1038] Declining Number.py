610d4128bfd53e63e950fa45068324b100146b52
ho991217 <ho991217@naver.com>
2022-01-11 13:56:57 +0900

    success: boj 1038 숫자를 배열로 변환해 다음 자리수가 더 크거나 같다면, 해당 자리수 수를 +1 후에 이후 수는 모두 0으로 변환 후 다음 연산 진행

--- BOJ/Algorithm/BruteForce Algorithm/[1038] Declining Number.py
+++ BOJ/Algorithm/BruteForce Algorithm/[1038] Declining Number.py
@@ -1,19 +1,34 @@
 import sys
 
 N = int(sys.stdin.readline())
-cache = []
 
-def isDecline(n):
-    number = str(n)
-    for i in range(len(number) - 1):
-        if int(number[i]) <= int(number[i + 1]):
-            return False
-    return True
+counter = -1
+number = 0
 
-counter = 0
-while counter <= 9876543210:
-    if isDecline(counter):
-        cache.append(counter)
-    counter += 1
+while True:
+    if number > 9876543210:
+        number = -1
+        break
+    string_num = [i for i in str(number)]
+    # print('current number: ', string_num)
+    isDecliningNumber = True
 
-print(cache)
+    for i in range(len(string_num) - 1):
+        if int(string_num[i]) <= int(string_num[i + 1]):
+            # print('not declining number, i = ', i ,end=' ')
+
+            string_num[i] = str(int(string_num[i]) + 1)
+            for j in range(i + 1, len(string_num)):
+                string_num[j] = '0'
+            # print(string_num)
+            isDecliningNumber = False
+
+    number = int(''.join(string_num))
+    if isDecliningNumber:
+        counter += 1
+        if counter == N:
+            break
+        else:
+            number += 1
+
+print(number)
