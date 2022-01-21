e585c0107782f5ce1d8607720e699e8b895676ac
ho991217 <ho991217@naver.com>
2022-01-21 15:29:21 +0900

    success: boj 1012

--- BOJ/Algorithm/DFS, BFS/[1012] Cabbage.py
+++ BOJ/Algorithm/DFS, BFS/[1012] Cabbage.py
@@ -11,26 +11,26 @@ def BFS(x, y):
     while q:
         x, y = q.popleft()
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0 <= nx < M and 0 <= ny < N and field[ny][nx][0] == 1 and field[ny][nx][1] is False:
-                field[ny][ny][1] = True
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < M and 0 <= ny < N and field[nx][ny][0] == 1 and field[nx][ny][1] is False:
+                field[nx][ny][1] = True
                 q.append((nx, ny))
 
 for _ in range(T):
     M, N, K = map(int, sys.stdin.readline().rstrip().split(' '))
 
-    field = [[[0, False] for x in range(M)] for j in range(N)]
+    field = [[[0, False] for i in range(N)] for j in range(M)]
+
     for i in range(K):
         x, y = map(int, sys.stdin.readline().rstrip().split(' '))
         field[x][y][0] = 1
 
-    # counter = 0
-    # for x in range(M):
-    #     for y in range(N):
-    #         if field[y][x][1] is False:
-    #             # BFS(x, y)
-    #             counter += 1
-    # print(counter)
+    counter = 0
+    for i in range(M):
+        for j in range(N):
+            if field[i][j][1] is False and field[i][j][0] == 1:
+                BFS(i, j)
+                counter += 1
 
-    print(field)
+    print(counter)
