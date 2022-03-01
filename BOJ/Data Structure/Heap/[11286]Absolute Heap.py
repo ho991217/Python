3b775c2a57ffe00ad6ec5708ca50d1b881299365
ho991217 <ho991217@naver.com>
2022-03-01 14:39:22 +0900

    failed: 11286 boj

--- BOJ/Data Structure/Heap/[11286]Absolute Heap.py
+++ BOJ/Data Structure/Heap/[11286]Absolute Heap.py
@@ -2,36 +2,63 @@ import sys
 import heapq
 
 N = int(sys.stdin.readline())
-heap = []
-check = dict()
+# heap = []
+# check = dict()
+#
+# for _ in range(N):
+#     cmd = int(sys.stdin.readline())
+#
+#     if cmd == 0:
+#         if heap == []:
+#             print(0)
+#         else:
+#             minAbs = heap[0]
+#             if check[minAbs][0] >= 1:
+#                 check[minAbs][0] -= 1
+#                 print(-minAbs)
+#             elif check[minAbs][1] >= 1:
+#                 check[minAbs][1] -= 1
+#                 print(minAbs)
+#             if check[minAbs] == [0, 0]:
+#                 heapq.heappop(heap)
+#
+#     else:
+#         absol = abs(cmd)
+#         if absol in heap:
+#             if cmd > 0:
+#                 check[absol][1] += 1
+#             else:
+#                 check[absol][0] += 1
+#         else:
+#             heapq.heappush(heap, absol)
+#             if cmd > 0:
+#                 check[absol] = [0, 1]
+#             else:
+#                 check[absol] = [1, 0]
+minusHeap = []
+plusHeap = []
 
 for _ in range(N):
     cmd = int(sys.stdin.readline())
 
     if cmd == 0:
-        if heap == []:
-            print(0)
+        if minusHeap and plusHeap:
+            if minusHeap[0] <= plusHeap[0]:
+                print(-heapq.heappop(minusHeap))
+            else:
+                print(heapq.heappop(plusHeap))
+
+        elif not minusHeap and plusHeap:
+            print(heapq.heappop(plusHeap))
+
+        elif not plusHeap and minusHeap:
+            print(-heapq.heappop(minusHeap))
+
         else:
-            minAbs = heap[0]
-            if check[minAbs][0] >= 1:
-                check[minAbs][0] -= 1
-                print(-minAbs)
-            elif check[minAbs][1] >= 1:
-                check[minAbs][1] -= 1
-                print(minAbs)
-            if check[minAbs] == [0, 0]:
-                heapq.heappop(heap)
+            print(0)
+
+    elif cmd < 0:
+        heapq.heappush(minusHeap, -cmd)
 
     else:
-        absol = abs(cmd)
-        if absol in heap:
-            if cmd > 0:
-                check[absol][1] += 1
-            else:
-                check[absol][0] += 1
-        else:
-            heapq.heappush(heap, absol)
-            if cmd > 0:
-                check[absol] = [0, 1]
-            else:
-                check[absol] = [1, 0]
+        heapq.heappush(plusHeap, cmd)
