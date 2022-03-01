e26d05c690984c4d2b0f9697e923578d7ee2ceb5
ho991217 <ho991217@naver.com>
2022-03-01 11:46:33 +0900

    succes: 1074 boj

--- BOJ/Algorithm/Div&Conquer/[1074] Z.py
+++ BOJ/Algorithm/Div&Conquer/[1074] Z.py
@@ -1,35 +1,23 @@
 import sys
 
-N, r, c = map(int, sys.stdin.readline().rstrip().split(' '))
+N, r, c = map(int, sys.stdin.readline().rstrip().split())
 
-graph = [[x + y for x in range(2**N)] for y in range(2**N)]
+def find(col, row, N, topCorner = 0):
+    if N == 0:
+        return topCorner
 
-def printGraph(graph):
-    for i in range(len(graph)):
-        for j in range(len(graph[i])):
-            print(graph[i][j], end=' ')
-        print()
+    half = 2 ** (N - 1)
+    if half > col and half <= row:
+        topCorner += half ** 2
+        row -= half
+    elif half <= col and half > row:
+        topCorner += 2 * (half ** 2)
+        col -= half
+    elif half <= col and half <= row:
+        topCorner += 3 * (half ** 2)
+        col -= half
+        row -= half
 
+    return find(col, row, N-1, topCorner)
 
-def search(target, r = 0, c = 0, counter = 0):
-    if target == [r, c]: return counter
-
-    for i in range(0, 3):
-        if i == 0:
-            c += 1
-        elif i == 1:
-            c -= 1
-            r += 1
-        else:
-            c += 1
-
-        counter += 1
-        print(f'[{r}, {c}] counter: {counter}')
-
-        if target == [r, c]: return counter
-
-    return search(target, r, c, counter)
-
-
-
-print(search([r, c]))
+print(find(r, c, N))
