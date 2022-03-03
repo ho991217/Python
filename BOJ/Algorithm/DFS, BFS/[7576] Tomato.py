a9c4a7637766f56c4953f224933eb1b810053eb7
ho991217 <ho991217@naver.com>
2022-03-03 12:57:58 +0900

    success: 7576 boj

--- BOJ/Algorithm/DFS, BFS/[7576] Tomato.py
+++ BOJ/Algorithm/DFS, BFS/[7576] Tomato.py
@@ -3,60 +3,38 @@ from collections import deque
 
 M, N = map(int, sys.stdin.readline().rstrip().split())
 
-box = [[int(i) for i in sys.stdin.readline().rstrip().split()] for j in range(N)]
-graph = dict()
-apart = False
-roast = []
+graph = []
+queue = deque()
+
 for i in range(N):
+    graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
     for j in range(M):
-        # 토마토가 칸에 들어 있는 경우
-        if box[i][j] >= 0:
-            if box[i][j] == 1: roast.append((i, j))
+        if graph[i][j] == 1:
+            queue.append([i, j])
 
-            graph[(i, j)] = []
-            if i-1 >= 0 and box[i-1][j] >= 0:
-                graph[(i, j)].append((i-1, j))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-            if j-1 >= 0 and box[i][j-1] >= 0:
-                graph[(i, j)].append((i, j-1))
-
-            if i+1 < N and box[i+1][j] >= 0:
-                graph[(i, j)].append((i+1, j))
-
-            if j+1 < M and box[i][j+1] >= 0:
-                graph[(i, j)].append((i, j+1))
-
-            # 인접 노드가 하나도 없을 경우
-            if graph[(i, j)] == []:
-                apart = True
-
-
-def bfs(graph, roasted):
-    if len(roasted) == len(list(graph.keys())):
-        return 0
-    elif apart:
-        return -1
-
-
-    day = 0
-    visited = roasted
-    queue = deque()
-    queue.append(roasted)
+def bfs():
     while queue:
-        nodes = queue.popleft()
-        if nodes == []: break
-        today_tomatoes = []
-        for cur in nodes:
-            for i in graph[cur]:
-                if i not in visited:
-                    today_tomatoes.append(i)
+        x, y = queue.popleft()
 
-        for u in today_tomatoes:
-            visited.append(u)
-        queue.append(today_tomatoes)
-        day += 1
+        for i in range(4):
+            a = x + dx[i]
+            b = y + dy[i]
+            if 0 <= a < N and 0 <= b < M and graph[a][b] == 0:
+                queue.append([a, b])
+                graph[a][b] = graph[x][y] + 1
 
 
-    return day-1
+day = 0
+bfs()
 
-print(bfs(graph, roast))
+for i in graph:
+    for j in i:
+        if j == 0:
+            print(-1)
+            exit(0)
+    day = max(day, max(i))
+print(day-1)
