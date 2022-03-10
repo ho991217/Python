bcb44807e3eb14e548760d9a5733b4411f256116
ho991217 <ho991217@naver.com>
2022-03-10 14:50:55 +0900

    success: 1149 boj

---
+++ BOJ/Data Structure/Linked List/Linked List.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# class LinkedList:
+#     def put(self, value):
+#
+#     def isEmpty(self):
+        
